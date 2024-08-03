f0c97cb882345318e37aad59d20a95843a93c87d
jlmuorizaga <jlmuorizaga@gmail.com>
2024-08-02 21:12:42 -0600

    Update regiones-ppal.page.ts

--- src/app/catalogos/regiones/regiones-ppal/regiones-ppal.page.ts
+++ src/app/catalogos/regiones/regiones-ppal/regiones-ppal.page.ts
@@ -38,6 +38,12 @@ export class RegionesPpalPage implements OnInit, OnDestroy {
 
   ngOnInit() {
     console.log('Entré a regiones en OnInit');
+    debugger
+    let x=0;
+    x=x+1;
+    x=x+3;
+    x=x*4;
+    x=x-10;
   }
   
   ngOnDestroy(): void {
