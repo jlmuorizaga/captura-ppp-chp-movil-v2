ac7d1e9fdadfd41911773270c0b4120792f5f876
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2024-09-04 22:56:45 -0600

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl:'http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com',
-  //baseUrl:'http://localhost',
+  //baseUrl:'http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com',
+  baseUrl:'http://localhost',
   puertoApiAdmonCatalogos:'3005',
   regiones:'/regiones',
   especialidades:'/especialidades',
