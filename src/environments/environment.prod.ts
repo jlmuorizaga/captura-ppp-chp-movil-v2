85094f937836c164590c41500a148f23317872eb
JOSE LUIS MU ORIZAGA <jlmuo@yahoo.com>
2024-11-20 15:29:20 -0600

    Update insertar-pizza

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  //baseUrl:'http://ec2-54-153-58-93.us-west-1.compute.amazonaws.com',
-  baseUrl:'http://ec2-54-144-58-67.compute-1.amazonaws.com',
-  //baseUrl:'http://localhost',
+  //baseUrl:'http://ec2-54-144-58-67.compute-1.amazonaws.com',
+  baseUrl:'http://localhost',
   puertoApiAdmonCatalogos:'3005',
   regiones:'/regiones',
   especialidades:'/especialidades',
