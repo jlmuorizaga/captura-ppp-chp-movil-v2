dcb2b970c6461177557dcb6ba902d3547ab724b3
jlmuorizaga <61767859+jlmuorizaga@users.noreply.github.com>
2026-02-22 19:44:21 -0600

    Update reformat relacion-orilla-sucursal-ppal y relacion-pizza-sucursal-ppal

--- src/app/catalogos/relacion-orilla-sucursal/relacion-orilla-sucursal-ppal/relacion-orilla-sucursal-ppal.page.ts
+++ src/app/catalogos/relacion-orilla-sucursal/relacion-orilla-sucursal-ppal/relacion-orilla-sucursal-ppal.page.ts
@@ -2,13 +2,33 @@ import { RelacionOrillaSucursalService } from './../../../services/relacion-oril
 import { RelacionOrillaSucursal } from './../../../model/dto/relacion-orilla-sucursal';
 
 import { SharedModule } from './../../../shared/shared/shared.module';
-import { ChangeDetectorRef,Component, OnInit,OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonIcon, IonButton,
-  IonBackButton, IonList, IonItem, IonLabel,AlertController, IonGrid, IonCol, IonRow, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonIcon,
+  IonButton,
+  IonBackButton,
+  IonList,
+  IonItem,
+  IonLabel,
+  AlertController,
+  IonGrid,
+  IonCol,
+  IonRow,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+} from '@ionic/angular/standalone';
 import { Subscription } from 'rxjs';
-import {filter} from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 import { NavigationEnd, Router } from '@angular/router';
 import { GlobalService } from 'src/app/services/global.service';
 import { SucursalService } from 'src/app/services/sucursal.service';
@@ -19,24 +39,46 @@ import { Sucursal } from 'src/app/model/dto/sucursal';
   templateUrl: './relacion-orilla-sucursal-ppal.page.html',
   styleUrls: ['./relacion-orilla-sucursal-ppal.page.scss'],
   standalone: true,
-  imports: [IonCardContent, IonCardTitle, IonCardSubtitle, IonCardHeader, IonCard, IonRow, IonCol, IonGrid, SharedModule,IonLabel, IonItem, IonList, IonBackButton, IonButton, IonIcon,
-    IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonCardContent,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonCard,
+    IonRow,
+    IonCol,
+    IonGrid,
+    SharedModule,
+    IonLabel,
+    IonItem,
+    IonList,
+    IonBackButton,
+    IonButton,
+    IonIcon,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
-export class RelacionOrillaSucursalPpalPage implements OnInit,OnDestroy {
-  navigationSubscription:Subscription;
-  relacionOrillaSucursal!:RelacionOrillaSucursal[];
-  mensaje:string;
-  idSucursal:string;
+export class RelacionOrillaSucursalPpalPage implements OnInit, OnDestroy {
+  navigationSubscription: Subscription;
+  relacionOrillaSucursal!: RelacionOrillaSucursal[];
+  mensaje: string;
+  idSucursal: string;
   cveSucursal!: string;
   sucursal!: Sucursal;
 
   constructor(
-    private relacionOrillaSucursalSvc:RelacionOrillaSucursalService,
-    private alertController:AlertController,
+    private relacionOrillaSucursalSvc: RelacionOrillaSucursalService,
+    private alertController: AlertController,
     private router: Router,
     private globalService: GlobalService,
     private sucursalesSvc: SucursalService,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
   ) {
     this.mensaje = 'Estoy en el constructor';
     this.idSucursal = this.globalService.idSucursalGlobal;
@@ -45,7 +87,7 @@ export class RelacionOrillaSucursalPpalPage implements OnInit,OnDestroy {
 
     console.log('Sucursal==>', this.globalService.idSucursalGlobal);
     this.navigationSubscription = this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
+      .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe(() => {
         this.leerRelacionOrillaSucursal(this.idSucursal);
       });
@@ -60,25 +102,25 @@ export class RelacionOrillaSucursalPpalPage implements OnInit,OnDestroy {
       this.navigationSubscription.unsubscribe();
     }
   }
-  leerRelacionOrillaSucursal(idSucursal:string){
-    this.relacionOrillaSucursalSvc.dameListaRelacionOrillaSucursal(idSucursal).subscribe({
-      next:(res:any)=>{
-        console.log('Servicio leido de forma exitosa')
-        console.log('claveSucursal=',idSucursal)
-        console.log(res);
-        this.relacionOrillaSucursal=res;
-        console.log(this.relacionOrillaSucursal);
-        this.relacionOrillaSucursal
-        this.cdr.detectChanges();
-
-      },
-      error:(error:any)=>{
-        console.log('Error en la lectura del servicio')
-        console.log(error)
-      }
-    })
+  leerRelacionOrillaSucursal(idSucursal: string) {
+    this.relacionOrillaSucursalSvc
+      .dameListaRelacionOrillaSucursal(idSucursal)
+      .subscribe({
+        next: (res: any) => {
+          console.log('Servicio leido de forma exitosa');
+          console.log('claveSucursal=', idSucursal);
+          console.log(res);
+          this.relacionOrillaSucursal = res;
+          console.log(this.relacionOrillaSucursal);
+          this.relacionOrillaSucursal;
+          this.cdr.detectChanges();
+        },
+        error: (error: any) => {
+          console.log('Error en la lectura del servicio');
+          console.log(error);
+        },
+      });
   }
-
 
   dameSucursal(idSucursal: string) {
     this.sucursalesSvc.dameSucursal(idSucursal).subscribe({
@@ -99,44 +141,57 @@ export class RelacionOrillaSucursalPpalPage implements OnInit,OnDestroy {
     });
   }
 
-  borraRegistroRelacionOrillaSucursal(idOrilla:string,idSucursal:string){
-    console.log('Voy a borrar este registro='+idOrilla+' '+idSucursal);
+  borraRegistroRelacionOrillaSucursal(idOrilla: string, idSucursal: string) {
+    console.log('Voy a borrar este registro=' + idOrilla + ' ' + idSucursal);
 
-    this.relacionOrillaSucursalSvc.borraRegistroRelacionOrillaSucursal(idOrilla,this.idSucursal).subscribe({
-      next:(res:any)=>{
-        console.log('Registro borrado de forma exitosa')
-        console.log(res);
-        this.leerRelacionOrillaSucursal(this.idSucursal);
-      },
-      error:(error:any)=>{
-        console.log('Error en el borrado del producto')
-        console.log(error)
-
-      }
-    })
-
+    this.relacionOrillaSucursalSvc
+      .borraRegistroRelacionOrillaSucursal(idOrilla, this.idSucursal)
+      .subscribe({
+        next: (res: any) => {
+          console.log('Registro borrado de forma exitosa');
+          console.log(res);
+          this.leerRelacionOrillaSucursal(this.idSucursal);
+        },
+        error: (error: any) => {
+          console.log('Error en el borrado del producto');
+          console.log(error);
+        },
+      });
   }
 
-  async confirmaBorrar(idOrilla:string, idSucursal:string,descripcion:string, tamanio:string, precio:string){
+  async confirmaBorrar(
+    idOrilla: string,
+    idSucursal: string,
+    descripcion: string,
+    tamanio: string,
+    precio: string,
+  ) {
     const alert = await this.alertController.create({
       header: 'Confirmación',
-      message: '¿Estás seguro de que deseas borrar el registro: '+descripcion+' '+tamanio+' con un precio de '+precio+'?',
+      message:
+        '¿Estás seguro de que deseas borrar el registro: ' +
+        descripcion +
+        ' ' +
+        tamanio +
+        ' con un precio de ' +
+        precio +
+        '?',
       buttons: [
         {
           text: 'Cancelar',
           role: 'cancel',
           handler: () => {
             console.log('Operación cancelada');
-          }
+          },
         },
         {
           text: 'Aceptar',
           handler: () => {
             console.log('Operación confirmada');
-            this.borraRegistroRelacionOrillaSucursal(idOrilla,idSucursal);
-          }
-        }
-      ]
+            this.borraRegistroRelacionOrillaSucursal(idOrilla, idSucursal);
+          },
+        },
+      ],
     });
 
     await alert.present();
@@ -145,22 +200,34 @@ export class RelacionOrillaSucursalPpalPage implements OnInit,OnDestroy {
     this.router.navigateByUrl('/insertar-relacion-orilla-sucursal');
   }
 
-  async saltaAEditarRegistroOrillaSucursal(idOrilla:string, idSucursal:string){
-    console.log('Estoy en editar registro orilla sucursal id='+idOrilla+' idSucursal='+idSucursal)
-    this.relacionOrillaSucursalSvc.dameRegistroRelacionOrillaSucursal(idOrilla,idSucursal).subscribe({
-      next:(res:any)=>{
-        console.log('Registro Relacion Orilla Sucursal regresada de forma exitosa')
-        console.log(res);
-        this.router.navigate(['/editar-relacion-orilla-sucursal'],{state:{data:res}});
-
-
-      },
-      error:(error:any)=>{
-        console.log('Error en la solicitud del registro relacion orilla sucursal')
-        console.log(error)
-
-      }
-    })
-
+  async saltaAEditarRegistroOrillaSucursal(
+    idOrilla: string,
+    idSucursal: string,
+  ) {
+    console.log(
+      'Estoy en editar registro orilla sucursal id=' +
+        idOrilla +
+        ' idSucursal=' +
+        idSucursal,
+    );
+    this.relacionOrillaSucursalSvc
+      .dameRegistroRelacionOrillaSucursal(idOrilla, idSucursal)
+      .subscribe({
+        next: (res: any) => {
+          console.log(
+            'Registro Relacion Orilla Sucursal regresada de forma exitosa',
+          );
+          console.log(res);
+          this.router.navigate(['/editar-relacion-orilla-sucursal'], {
+            state: { data: res },
+          });
+        },
+        error: (error: any) => {
+          console.log(
+            'Error en la solicitud del registro relacion orilla sucursal',
+          );
+          console.log(error);
+        },
+      });
   }
 }
